0be0a2bc1f764174143a9425400bc198093222bb
Arjuna S. Erat <erat.simon@gmail.com>
2013-10-28 01:03:06 +0100

    Fixed changebg.sh to use TUI rather than script-tools

--- scripts/changebg.sh
+++ scripts/changebg.sh
@@ -6,30 +6,31 @@
 #
 #	Author: 		Simon A. Erat (sea) <erat.simon AT gmail.com>
 #	Created (y.m.d):	2012.10.24
-#	Changed:		2013.02.02
+#	Changed:		2013.10.26
 #	License:		GPL v3
-	script_version=0.2
+	script_version=0.3
 #
 #	Variables
 #
-	stconf=/usr/share/script-tools/st.cfg
-	[ ! -f $stconf ] && \
-		echo "This script was written for the sea-Awesome-WM compilation." && \
-		echo "Therefor it requires sea's Script-Tools." && \
-		echo "Please have a look at: http://sea.hostingsociety.com/?p=Script-Tools" && \
-		exit 1 || \
-		source $stconf
+	#stconf=/usr/share/script-tools/st.cfg
+	#[ ! -f $stconf ] && \
+	#	echo "This script was written for the sea-Awesome-WM compilation." && \
+	#	echo "Therefor it requires sea's Script-Tools." && \
+	#	echo "Please have a look at: http://sea.hostingsociety.com/?p=Script-Tools" && \
+	#	exit 1 || \
+	#	source $stconf
 	
-	defaultbg=$HOME/.config/awesome/img/bg
+	defaultbg=$HOME/.config/awesome/img
 	nasabg=$HOME/.backgrounds
 	
-	str_kind=" Select kind of background:"
+	str_kind="Select kind of background:"
 	str_bg="Select the actual background image:"
 #
 #	Outout
 #	
-	sH
-	sT "$str_kind"
+	printf "\r\t\r";echo
+	tui-header "sea's Awesome WM" "Change BG"
+	tui-title "$str_kind"
 	select style in Back NASA Defaults Custom
 	do	case $style in
 		Back)		break		;;
@@ -38,14 +39,14 @@
 		Custom) 	path=$(input "Please type the absolute path of the images:") ;;
 		esac
 		
-		sT "$str_bg"
-		sE "$style" "$path"
+		tui-title "$str_bg"
+		tui-echo "$style" "$path"
 		pics=$(cd $path;ls $path)
 		select img in Back $pics
 		do	[ $img = Back ] && break
 			feh --bg-scale $path/$img
-			sE "$str_bg" "or go Back"
+			tui-echo "$str_bg" "or go Back"
 		done
 	
-		sE "$str_kind" "or go Back to exit."
+		tui-echo "$str_kind" "or go Back to exit."
 	done
